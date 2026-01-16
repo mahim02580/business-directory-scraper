028f51c997aec33f3cbad002a264ac01a7bd78a0
Mahim <mahim02580@gmail.com>
2026-01-16 21:12:29 +0600

    Add rate limiting to prevent excessive requests

--- main.py
+++ main.py
@@ -1,55 +1,74 @@
 import time
+import random
 import requests
-from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 base_url = "https://www.bdbusinessdirectory.com"
 endpoint = "/single-location/all-businesses-in-dhaka/"
 
+headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/120.0.0.0 Safari/537.36"
+    ),
+    "Accept-Language": "en-US,en;q=0.9",
+}
+
 page_num = 1
 business_info_list = []
-start_time = time.time()
+start_time = datetime.now()
+print("Starting...")
 while True:
-    page = requests.get(f"{base_url}{endpoint}", params={"page": page_num, "directory_type": "general"})
+    print(f"Scraping page number - {page_num}")
+    page = requests.get(f"{base_url}{endpoint}", params={"page": page_num, "directory_type": "general"},
+                        headers=headers)
+    time.sleep(random.uniform(2, 5))  # A random delay to incorporate human-like behaviour
 
-    soup = BeautifulSoup(page.content, "html.parser")
+    soup = BeautifulSoup(page.text, "html.parser")
 
     cards_links_elements = soup.select(selector="h2.directorist-listing-title a")
+
     if not cards_links_elements:
-        break
+        break  # Ends loop if there is no more business information cards on the page
     cards_links = [cards_links_element.get("href") for cards_links_element in cards_links_elements]
 
-    for link in cards_links:
-        business_details_page = requests.get(link)
-        business_details_soup = BeautifulSoup(business_details_page.content, "html.parser")
+    for card_link in cards_links:
+        business_details_page = requests.get(card_link, headers=headers)
+        time.sleep(random.uniform(2, 5))  # A random delay to incorporate human-like behaviour
 
-        business_title = business_details_soup.find("h1", class_="directorist-listing-details__listing-title").text
+        business_details_soup = BeautifulSoup(business_details_page.text, "html.parser")
 
-        business_address = business_details_soup.select_one(
+        title_tag = business_details_soup.find("h1", class_="directorist-listing-details__listing-title")
+        title = title_tag.text.strip() if title_tag else "N/A"
+
+        address_tag = business_details_soup.select_one(
             "div.directorist-single-info.directorist-single-info-address div.directorist-single-info__value")
-        address = business_address.text.strip() if business_address else "N/A"
+        address = address_tag.text.strip() if address_tag else "N/A"
 
-        business_phone = business_details_soup.select_one(
+        phone_tag = business_details_soup.select_one(
             "div.directorist-single-info.directorist-single-info-phone div.directorist-single-info__value")
-        phone = business_phone.text.replace("-", "").strip().replace(" ", "") if business_phone else "N/A"
+        # Data Cleaning
+        phone = phone_tag.text.replace("-", "").strip().replace(" ", "") if phone_tag else "N/A"
         if phone != "N/A":
             if ":" in phone:
                 phone = phone.split(",")[0].split(":")[1].strip()
             if "+88" not in phone:
                 phone = "+88" + phone
 
-
-        business_email = business_details_soup.select_one(
+        email_tag = business_details_soup.select_one(
             "div.directorist-single-info.directorist-single-info-email div.directorist-single-info__value")
-        email = business_email.text.strip() if business_email else "N/A"
+        email = email_tag.text.strip() if email_tag else "N/A"
 
-        business_website = business_details_soup.select_one(
+        website_tag = business_details_soup.select_one(
             "div.directorist-single-info.directorist-single-info-web div.directorist-single-info__value")
-        website = business_website.text.strip() if business_website else "N/A"
+        website = website_tag.text.strip() if website_tag else "N/A"
 
-        business_zip = business_details_soup.select_one(
+        zipcode_tag = business_details_soup.select_one(
             "div.directorist-single-info.directorist-single-info-zip div.directorist-single-info__value")
-        zip = business_zip.text.strip() if business_zip else "N/A"
+        zipcode = zipcode_tag.text.strip() if zipcode_tag else "N/A"
 
         business_social_links = business_details_soup.select_one(
             "div.directorist-single-info.directorist-single-info-socials div.directorist-social-links")
@@ -72,12 +91,12 @@ while True:
             linkedin_link = "N/A"
 
         business_info = {
-            "Business Name": business_title,
+            "Business Name": title,
             "Phone": phone,
             "Website": website,
             "Email": email,
             "Address": address,
-            "Zip Code": zip,
+            "Zip Code": zipcode,
             "Facebook": facebook_link,
             "Instagram": instagram_link,
             "Youtube": youtube_link,
@@ -89,11 +108,18 @@ while True:
     if page_num == 10:
         break
 
-business_df = pd.DataFrame(business_info_list)
-business_df.to_excel("sample_data.xlsx", index=False)
+print("Scraping Ended!")
 
+print("Exporting to excel...")
+data_df = pd.DataFrame(business_info_list)
+data_df.to_excel("data.xlsx", index=False)
+print("Done! Check \"data.xlsx\"")
 
-end_time = time.time()
-
-diff_time = divmod((end_time - start_time), 60)
-print(f"Time taken: {diff_time[0]}m {diff_time[1]}s")
+# Runtime Calculation
+end_time = datetime.now()
+diff = end_time - start_time
+total_seconds = int(diff.total_seconds())
+hours = total_seconds // 3600
+minutes = (total_seconds % 3600) // 60
+seconds = total_seconds % 60
+print(f"Time taken: {hours}h {minutes}m {seconds}s")
